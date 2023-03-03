811aae7118230618a402e88b2ffae889d5226c73
Digetix <svet.love.234@gmail.com>
2023-03-04 00:49:40 +0200

    Логика

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-const select = document.getElementById('terms-of-payment');
-const termDetails = document.getElementById('term-details');
-const dateContract = document.getElementById("myDate");
-const datePay = document.getElementById("date-pay");
+const select = document.getElementById("terms-of-payment");
+			termDetails = document.getElementById("term-details");
+			dateContract = document.getElementById("myDate");
+			datePay = document.getElementById("date-pay");
+			checkBox = document.getElementById("chkbx-without-install");
+			saveActBtn = document.getElementById("buttons-act");
+			saveContractBtn = document.getElementById("btn-save-contract");
+			priceContract = document.getElementById("price");
+			pricePay = document.getElementById("price-pay-contract");
 
-let date = new Date(),
-		dayOfDate = date.getDate(),
+let date = new Date();
+		dayOfDate = date.getDate(); 
+			if (dayOfDate<10) dayOfDate='0'+dayOfDate;
 		month = date.getMonth()+1;
+			if (month<10) month='0'+month;
 		year = date.getFullYear();
 
-		if (month < 10) {
-				month = '0'+month;
-		}
+dateContract.value = year+"-"+month+"-"+dayOfDate;
 
-dateContract.value = year+'-'+month+'-'+dayOfDate;
+priceContract.addEventListener('change', function handleChange(event) {
+		pricePay.value = Math.round((priceContract.value / 100) * 80);
+	}
+);
 
 select.addEventListener('change', function handleChange(event) {
 	if (select.options[select.selectedIndex].value == 'value1') {
 		termDetails.style.visibility = "hidden";
 	} else {
-		termDetails.style.visibility = "visible";	
-		datePay.value = year+'-'+month+'-'+dayOfDate;
+		termDetails.style.visibility = "visible";
+		datePay.value = dateContract.value;
+		pricePay.value = Math.round((priceContract.value / 100) * 80);
 	}
 });
 
+function myFunction() {
+	if (checkBox.checked == true){
+		saveActBtn.style.visibility = "hidden";
+		saveContractBtn.textContent = 'Договор без монтажа';
+	} else {
+		saveActBtn.style.visibility = "visible";
+		saveContractBtn.textContent = 'Договор';
+	}
+}
 
 
 
